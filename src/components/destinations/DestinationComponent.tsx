a46ffa8c91c4bf9cd4dceca3a236c917393c1c26
Bosco <nzibosco@gmail.com>
2020-02-16 22:13:12 -0500

    jumbotron display

--- src/components/destinations/DestinationComponent.tsx
+++ src/components/destinations/DestinationComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../../utils/navbar/Navbar';
 import { Jumbotron } from 'reactstrap';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 
@@ -18,67 +18,69 @@ export class DestinationComponent extends React.Component<IDestinationProps, IDe
 
     constructor(props: any) {
         super(props)
-        this.state={
+        this.state = {
             //planets: []
         }
     }
 
-    async componentDidMount(){
-        if(this.props.allDestinations.length === 0) {
+    async componentDidMount() {
+        if (this.props.allDestinations.length === 0) {
             this.props.getAllPlanets()
-            
+
         }
     }
 
 
 
-    render(){
-        const displayPlanets = this.props.allDestinations.map((planet:any) => {
+    render() {
+        const displayPlanets = this.props.allDestinations.map((planet: any) => {
             return (
-                <div className = "card">
-                    <h4>{planet.englishName}</h4>
-            <p><strong>Aphelion: </strong>{planet.aphelion}</p>
-            <p><strong>Gravity: </strong>{planet.gravity}</p>
-            <p><strong>Discovery Date: </strong>{planet.discoveryDate}</p>
-            <p><strong>Discovered By: </strong>{planet.discoveredBy}</p>
-        
-                    {planet.moons?(
-                        <div>
-                            <strong>Moons:</strong>                           
-                            {
-                                planet.moons.map((m:any) => {                               
-                                    return (
-                                    <p>{m.moon}</p>
-                                    )
-                                })                           
-                            }
-                        </div>
-                        
-                    ):(<p>This planet has no moons</p>)}
-                    <br></br>
-                    
-                    <Link to = "/schedule-flight">Schedule a flight to this planet</Link>
-                    <br></br>
+                <div className="jumbotron">
+                    <div className="jumbotron">
+                        <h4>{planet.englishName}</h4>
+                        <p><strong>Aphelion: </strong>{planet.aphelion}</p>
+                        <p><strong>Gravity: </strong>{planet.gravity}</p>
+                        <p><strong>Discovery Date: </strong>{planet.discoveryDate}</p>
+                        <p><strong>Discovered By: </strong>{planet.discoveredBy}</p>
+
+                        {planet.moons ? (
+                            <div>
+                                <strong>Moons:</strong>
+                                {
+                                    planet.moons.map((m: any) => {
+                                        return (
+                                            <p>{m.moon}</p>
+                                        )
+                                    })
+                                }
+                            </div>
+
+                        ) : (<p>This planet has no moons</p>)}
+                        <br></br>
+
+                        <Link to="/schedule-flight">Schedule a flight to this planet</Link>
+                        <br></br>
+                    </div>
                 </div>
             )
         })
 
-        if(this.props.allDestinations){
-        return (
-            <>
-            <Navbar/>
-           
-             {displayPlanets}
-            </>
-        )
-        } else {
+        if (this.props.allDestinations) {
             return (
-                
-                <>                
-                <p>Sorry, we are unable to get you some planets</p>
-                <Link to = "/">Click to go back to home page</Link>
+                <>
+                    <Navbar />
+
+                    {displayPlanets}
                 </>
             )
-        }        
+        } else {
+            return (
+
+                <>
+                    <p>Sorry, we are unable to get you some planets</p>
+                    <Link to="/">Click to go back to home page</Link>
+                </>
+            )
+        }
     }
 }
