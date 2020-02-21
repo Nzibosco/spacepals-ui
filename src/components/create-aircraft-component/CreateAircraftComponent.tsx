5b35948017cacbdaaef6e9de444f75c472f16d95
Bosco <nzibosco@gmail.com>
2020-02-20 20:10:22 -0500

    updated dev

---
+++ src/components/create-aircraft-component/CreateAircraftComponent.tsx
@@ -0,0 +1,117 @@
+import React, { SyntheticEvent } from 'react';
+import SpaceNav from '../../utils/navbar/Navbar';
+import { Form, FormGroup, Label, Col, Input, Button } from 'reactstrap';
+import Axios from 'axios';
+
+export class CreateAircraftComponent extends React.Component<any,any> {
+    constructor(props: any) {
+        super(props)
+        this.state={
+            name:'',
+            size:'',
+            cid:0
+        }
+
+    }
+
+    updateSize = (event:any) => {
+        this.setState({
+            ...this.state,
+            size:event.target.value
+        })
+    }
+
+    updateName = (event:any) => {
+        this.setState({
+            ...this.state,
+            name:event.target.value
+        })
+    }
+
+    updateID = (event:any) => {
+        this.setState({
+            ...this.state,
+            cid:event.target.value
+        })
+    }
+
+    submitShip = (event: SyntheticEvent) => {
+        event.preventDefault()
+        let aircraft = {
+            name:this.state.name,
+            capacity:this.state.size
+        }
+        let shipdto = {
+            id:this.state.cid,
+            aircraft:aircraft
+        }
+        console.log(shipdto)
+        Axios.post('http://localhost:8080/spacepals/aircrafts', shipdto).then(res => {
+            console.log(res)
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <SpaceNav/>
+                <div style = {{
+                    margin:"10% 30%"
+                }}>
+                    <div style={{
+                    backgroundColor:"white",
+                    outlineWidth:"2px",
+                    outlineColor:"black"
+                }}>
+                    <h1>Create your Ship</h1>
+                    <br></br>
+                    <br></br>
+                    <Form onSubmit={this.submitShip}>
+                        <FormGroup row>
+                            <Label for="exampleShipname" sm={2}>Ship Name</Label>
+                            <Col sm={10}>
+                                <Input required
+                                    type="text"
+                                    name="shipname"
+                                    id="exampleShipname"
+                                    placeholder="put ship name here"
+                                    value={this.state.name}
+                                    onChange={this.updateName} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="companyID" sm={2}>Company ID</Label>
+                            <Col sm={10}>
+                                <Input required
+                                    type="number"
+                                    name="companyID"
+                                    id="exampleCompanyID"
+                                    placeholder="company id"
+                                    value={this.state.cid}
+                                    onChange={this.updateID} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="exampleSize" sm={2}>Capacity</Label>
+                            <Col sm={10}>
+                            <Input required
+                                type="select" 
+                                name="select" 
+                                id="exampleSelect"
+                                value={this.state.size}
+                                onChange={this.updateSize} >
+                                <option >SMALL</option>
+                                <option>MEDIUM</option>
+                                <option>LARGE</option>
+                            </Input>
+                            </Col>
+                        </FormGroup>
+                        <Button color="primary">Create Ship</Button>
+                    </Form>
+                    <p>{this.props.loginMessage}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/components/create-flight-component/CreateFlightComponent.tsx
@@ -0,0 +1,155 @@
+import React, { SyntheticEvent } from 'react'
+import { Form, FormGroup, Label, Col, Input, Button } from 'reactstrap'
+import SpaceNav from '../../utils/navbar/Navbar'
+import Axios from 'axios'
+
+export class CreateFlightComponent extends React.Component<any,any> {
+    constructor(props:any) {
+        super(props)
+        this.state= {
+            cid:0,      //company id
+            sid:0,      //ship id
+            dept:'',    //departure
+            dest:'',    //destination
+            cost:0,     //cost
+
+        }
+    }
+
+    updateCid = (event:any) => {
+        this.setState({
+            ...this.state,
+            cid:event.target.value
+        })
+    }
+
+    updateSid = (event:any) => {
+        this.setState({
+            ...this.state,
+            sid:event.target.value
+        })
+    }
+    updateDept = (event:any) => {
+        this.setState({
+            ...this.state,
+            dept:event.target.value
+        })
+    }
+    updateDest = (event:any) => {
+        this.setState({
+            ...this.state,
+            dest:event.target.value
+        })
+    }
+    updateCost = (event:any) => {
+        this.setState({
+            ...this.state,
+            cost:event.target.value
+        })
+    }
+
+    submitFlight = (event: SyntheticEvent) => {
+        event.preventDefault()
+        let flight = {
+            //departure:this.state.dept,
+            //destination:this.state.dest,
+            cost:this.state.cost
+        }
+        let flightdto = {
+            companyId:this.state.cid,
+            shipId:this.state.sid,
+            flight:flight
+        }
+        console.log(flightdto)
+        Axios.post('http://localhost:8080/spacepals/flights', flightdto).then(res => {
+            console.log(res);
+        })
+    }
+
+
+
+    render() {
+        return(
+            <div>
+                <SpaceNav/>
+                <div style = {{
+                    margin:"10% 30%"
+                }}>
+                    <div style={{
+                    backgroundColor:"white",
+                    outlineWidth:"2px",
+                    outlineColor:"black"
+                }}>
+                    <h1>Welcome</h1>
+                    <br></br>
+                    <br></br>
+                    <Form onSubmit={this.submitFlight}>
+                        <FormGroup row>
+                            <Label for="exampleCid" sm={2}>Company ID</Label>
+                            <Col sm={10}>
+                                <Input required
+                                    type="number"
+                                    name="cid"
+                                    id="exampleCid"
+                                    placeholder="put company ID here"
+                                    value={this.state.cid}
+                                    onChange={this.updateCid} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="exampleSid" sm={2}>Ship ID</Label>
+                            <Col sm={10}>
+                                <Input required
+                                    type="number"
+                                    name="sid"
+                                    id="exampleSid"
+                                    placeholder="put ship id here"
+                                    value={this.state.sid}
+                                    onChange={this.updateSid} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="exampleDeparture" sm={2}>Departure</Label>
+                            <Col sm={10}>
+                                <Input required
+                                    type="text"
+                                    name="departure"
+                                    id="exampleDeparture"
+                                    placeholder="put departure here"
+                                    value={this.state.dept}
+                                    onChange={this.updateDept} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="exampleDestination" sm={2}>Destination</Label>
+                            <Col sm={10}>
+                                <Input required
+                                    type="text"
+                                    name="destination"
+                                    id="exampleDestination"
+                                    placeholder="put destination here"
+                                    value={this.state.dest}
+                                    onChange={this.updateDest} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="exampleCost" sm={2}>Cost</Label>
+                            <Col sm={10}>
+                                <Input required
+                                    type="number"
+                                    name="cost"
+                                    id="exampleCost"
+                                    placeholder="put cost here"
+                                    value={this.state.cost}
+                                    onChange={this.updateCost} />
+                            </Col>
+                        </FormGroup>
+                        <Button color="primary">Create Flight</Button>
+                    </Form>
+                    <p>{this.props.loginMessage}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
