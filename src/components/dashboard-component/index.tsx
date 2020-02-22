ce0d62b501e0524f5d4c52b0aff9ec13ff35a978
Evan Hsi <evanhsi@Evans-MacBook-Pro.local>
2020-02-22 15:15:46 -0600

    updating index dashboard

--- src/components/dashboard-component/index.tsx
+++ src/components/dashboard-component/index.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import {ManagerDashboard} from './managers/ManagerDashboard';
 import {DashboardComponent} from './regular-user/DashboardComponent';
+import { Redirect } from 'react-router';
 
 // this file will track user roles and render dashboards accordingly.
 // if a user is a company manager, the dashboard rendered should be imported from managers folder and vice versa
 
 interface IDashboardState {
-    role: string
-
-
 }
 
 interface IDashboardProps {
@@ -21,25 +19,17 @@ export class Dashboard extends React.Component<IDashboardProps, IDashboardState>
 
     constructor(props:IDashboardProps){
         super(props)
-        this.state ={
-            role: ''
-        }
     }
 
-render(){
-    console.log(this.props.currentUser)
-    return(
-        <>
+    render(){
 
-            {/* {
-                this.props.currentUser.role === "basic_user" ? 
-                <DashboardComponent/> :
-                <ManagerDashboard/>
-            } */}
-
-        </>
-    )
-}
+        console.log(this.props.currentUser)
+        if(!this.props.currentUser)
+            return <Redirect to='/login'/>
+        if(this.props.currentUser.role === "Flight_Manager")
+            return <Redirect to='/mandash'/>
+        else return <Redirect to='/userdash'/>
+    }
 
 
 }
