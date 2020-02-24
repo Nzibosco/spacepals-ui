b986abb615bb0630f669db223cdd8abbefca6c86
Bosco <nzibosco@gmail.com>
2020-02-24 00:59:49 -0500

    most of the things

--- src/components/create-flight-component/CreateFlightComponent.tsx
+++ src/components/create-flight-component/CreateFlightComponent.tsx
@@ -2,30 +2,31 @@ import React, { SyntheticEvent } from 'react'
 import { Form, FormGroup, Label, Col, Input, Button } from 'reactstrap'
 import SpaceNav from '../../utils/navbar/Navbar'
 import Axios from 'axios'
-interface newFlightState{
-    planetbody:[]
+import { Redirect } from 'react-router'
+interface newFlightState {
+    planetbody: []
     aircraftbody: [],
     selectedDestId: any,
     selectedDeptId: any,
     aircraftId: any,
     cost: any
 }
-interface newFlightProps{
+interface newFlightProps {
     currentUser: any
 }
-export class CreateFlightComponent extends React.Component<newFlightProps,newFlightState> {
-    constructor(props:any) {
+export class CreateFlightComponent extends React.Component<newFlightProps, newFlightState> {
+    constructor(props: any) {
         super(props)
-        this.state= {
-            aircraftbody:[],
-            planetbody:[],      //company id
-            aircraftId:'',      //ship id
-            selectedDeptId:'',    //departure
-            selectedDestId:'',    //destination
-            cost:'',     //cost
+        this.state = {
+            aircraftbody: [],
+            planetbody: [],      //company id
+            aircraftId: '',      //ship id
+            selectedDeptId: '',    //departure
+            selectedDestId: '',    //destination
+            cost: '',     //cost
         }
     }
-    updateAircraftId = (event:any) => {
+    updateAircraftId = (event: any) => {
         this.setState({
             ...this.state,
             aircraftId: event.target.value
@@ -41,29 +42,29 @@ export class CreateFlightComponent extends React.Component<newFlightProps,newFli
                 })
             }).catch(err => console.log(err)
             )
-            
-            
-                
-        
+
+
+
+
     }
-    
-    
-    updateSelectedDeptId = (event:any) => {
+
+
+    updateSelectedDeptId = (event: any) => {
         this.setState({
             ...this.state,
-            selectedDeptId:event.target.value
+            selectedDeptId: event.target.value
         })
     }
-    updateSelectedDestId = (event:any) => {
+    updateSelectedDestId = (event: any) => {
         this.setState({
             ...this.state,
-            selectedDestId:event.target.value
+            selectedDestId: event.target.value
         })
     }
-    updateCost = (event:any) => {
+    updateCost = (event: any) => {
         this.setState({
             ...this.state,
-            cost:event.target.value
+            cost: event.target.value
         })
     }
     submitFlight = (event: SyntheticEvent) => {
@@ -71,13 +72,13 @@ export class CreateFlightComponent extends React.Component<newFlightProps,newFli
         let flight = {
             //departure:this.state.dept,
             //destination:this.state.dest,
-            cost:this.state.cost
+            cost: this.state.cost
         }
         let flightdto = {
             cost: this.state.cost,
-            selectedDeptId:this.state.selectedDeptId,
-            selectedDestId:this.state.selectedDestId,
-            aircraftId:this.state.aircraftId
+            selectedDeptId: this.state.selectedDeptId,
+            selectedDestId: this.state.selectedDestId,
+            aircraftId: this.state.aircraftId
         }
         console.log(flightdto)
         Axios.post('http://projecttwodo-env.fryh9swbjr.us-east-2.elasticbeanstalk.com/flights', flightdto).then(res => {
@@ -85,22 +86,23 @@ export class CreateFlightComponent extends React.Component<newFlightProps,newFli
         })
     }
     render() {
-        return(
+        if(this.props.currentUser === null) return <Redirect to ="/login"/>
+        return (
             <div>
                 {/* <SpaceNav/> */}
-                <div style = {{
-                    margin:"10% 30%"
+                <div style={{
+                    margin: "10% 30%"
                 }}>
                     <div style={{
-                    backgroundColor:"inherit",
-                    outlineWidth:"2px",
-                    outlineColor:"black", color: "white"
-                }}>
-                    <h1>Welcome, Please Set Your Flight Parameters.</h1>
-                    <br></br>
-                    <br></br>
-                    <Form onSubmit={this.submitFlight}>
-                        {/* <FormGroup row>
+                        backgroundColor: "inherit",
+                        outlineWidth: "2px",
+                        outlineColor: "black", color: "white"
+                    }}>
+                        <h1>Welcome, Please Set Your Flight Parameters.</h1>
+                        <br></br>
+                        <br></br>
+                        <Form onSubmit={this.submitFlight}>
+                            {/* <FormGroup row>
                             <Label for="exampleCid" sm={2}>Company ID</Label>
                             <Col sm={10}>
                                 <Input required
@@ -112,62 +114,62 @@ export class CreateFlightComponent extends React.Component<newFlightProps,newFli
                                     onChange={this.updateCid} />
                             </Col>
                         </FormGroup> */}
-                        <FormGroup row>
-                            <Label for="exampleSid" sm={2}>Select an Aircraft</Label>
-                            <Col sm={10}>
-                             <select onChange={this.updateAircraftId} required>
-                        <option placeholder="select aircraft ..."></option>
-                        {
-                            this.props.currentUser.companies[0].aircrafts.map((aircraft: any, i: any) => {
-                            return <option value={aircraft.id} id={i}>{aircraft.name} The Flight capacity is:{aircraft.capacity}</option>
-                            })})
-                        }
+                            <FormGroup row>
+                                <Label for="exampleSid" sm={2}>Select an Aircraft</Label>
+                                <Col sm={10}>
+                                    <select onChange={this.updateAircraftId} required>
+                                        <option placeholder="select aircraft ..."></option>
+                                        {
+                                            this.props.currentUser.companies[0].aircrafts.map((aircraft: any, i: any) => {
+                                                return <option value={aircraft.id} id={i}>{aircraft.name} The Flight capacity is:{aircraft.capacity}</option>
+                                            })})
+                                    }
                     </select>
-                    </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Label for="Departure" sm={2}>Select a Departure Planet</Label>
-                            <Col sm={10}>
-                            <select onChange={this.updateSelectedDeptId} required>
-                        <option placeholder="Select Departure Planet ..."></option>
-                        {
-                            this.state.planetbody.map((planet: any, i: any) => {
-                                return <option value={planet.id} id={i}>{planet.planetName}</option>
-                            })
-                        }
-                    </select>
-                    </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Label for="Destination" sm={2}>Select a Destination Planet</Label>
-                            <Col sm={10}>
-                            <select onChange={this.updateSelectedDestId} required>
-                        <option placeholder="Select Destination Planet ..."></option>
-                        {
-                            this.state.planetbody.map((planetdest: any, i: any) => {
-                                return <option value={planetdest.id} id={i}>{planetdest.planetName}</option>
-                            })
-                        }
-                    </select>
-                    </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Label for="exampleCost" sm={2}>Cost</Label>
-                            <Col sm={10}>
-                                <Input required
-                                    type="number"
-                                    name="cost"
-                                    id="exampleCost"
-                                    placeholder="Set the Flight Cost"
-                                    value={this.state.cost}
-                                    onChange={this.updateCost} />
-                            </Col>
-                        </FormGroup>
-                        <Button color="primary">Create Flight</Button>
-                    </Form>
-                    {/* <p>{this.props.loginMessage}</p> */}
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label for="Departure" sm={2}>Select a Departure Planet</Label>
+                                <Col sm={10}>
+                                    <select onChange={this.updateSelectedDeptId} required>
+                                        <option placeholder="Select Departure Planet ..."></option>
+                                        {
+                                            this.state.planetbody.map((planet: any, i: any) => {
+                                                return <option value={planet.id} id={i}>{planet.planetName}</option>
+                                            })
+                                        }
+                                    </select>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label for="Destination" sm={2}>Select a Destination Planet</Label>
+                                <Col sm={10}>
+                                    <select onChange={this.updateSelectedDestId} required>
+                                        <option placeholder="Select Destination Planet ..."></option>
+                                        {
+                                            this.state.planetbody.map((planetdest: any, i: any) => {
+                                                return <option value={planetdest.id} id={i}>{planetdest.planetName}</option>
+                                            })
+                                        }
+                                    </select>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label for="exampleCost" sm={2}>Cost</Label>
+                                <Col sm={10}>
+                                    <Input required
+                                        type="number"
+                                        name="cost"
+                                        id="exampleCost"
+                                        placeholder="Set the Flight Cost"
+                                        value={this.state.cost}
+                                        onChange={this.updateCost} />
+                                </Col>
+                            </FormGroup>
+                            <Button color="primary">Create Flight</Button>
+                        </Form>
+                        {/* <p>{this.props.loginMessage}</p> */}
                     </div>
-    <p>Aircraft Id:{this.state.aircraftId}  Departure:{this.state.selectedDeptId} Destination:{this.state.selectedDestId}</p>
+                    <p>Aircraft Id:{this.state.aircraftId}  Departure:{this.state.selectedDeptId} Destination:{this.state.selectedDestId}</p>
                 </div>
             </div>
         )
